747431ca89b4f271d7818991af72a6cf6906e478
Hidetaka Miyamura <contato@hideartz.com>
2023-10-25 05:58:21 -0300

    upd NPCTalking & models
    Importado modelos das atendentes

---
+++ HorroRAM/Assets/Scripts/NPCBehaviours/NPCTalking.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTalking: MonoBehaviour
+{
+    private Animator animator;
+    private bool playerInTrigger = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (playerInTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetBool("isTalking", true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+        }
+    }
+}
